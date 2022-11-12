f48be6e99dcb841225383e57f505909b44ff3e62
wenjie <wenjieb2@gmail.com>
2022-11-13 01:21:51 +0800

    Implement error handling and dom manipulation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import './style.css'
 
 async function fetchData (country) {
   try {
-    const rawData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}+&appid=6c6ad05549d59a3d691d1888c65dc8b3`)
+    const rawData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}+&appid=6c6ad05549d59a3d691d1888c65dc8b3&units=metric`)
     const weatherData = await rawData.json()
-    //  handle no response
-    if (weatherData.cod === '404') { alert('nope') }
     return weatherData
   } catch (err) {
     alert('sorry the api broke bro :(')
@@ -14,9 +12,47 @@ async function fetchData (country) {
 
 function processData (country) {
   fetchData(country)
-    .then(data => console.log(data))
+    .then(function(data){ 
+      try {
+        console.log(data)
+        let temperature = Math.round(data.main.temp)
+        let name = data.name
+        let description = data.weather[0].description
+        let feels = Math.round(data.main['feels_like'])
+        let humidity = data.main.humidity
+        let wind = data.wind.speed
+        editDom(temperature, name, description, feels, humidity, wind)
+      } catch {
+        const error = document.querySelector('.error')
+        error.style.display = 'block'
+        setTimeout(()=>{
+          error.style.display ='none'
+        }, 2000)
+      }
+    })
 }
 
+function editDom (temperature, name, description, feels, humidity, wind) {
+  const temperaturetext = document.querySelector('.temperature')
+  const city = document.querySelector('.city')
+  const describe = document.querySelector('.description')
+  const feelslike = document.querySelector('.feelsLike')
+  const humidtext = document.querySelector('.humidity')
+  const windspeed = document.querySelector('.wind')
+  temperaturetext.textContent = temperature
+  temperaturetext.innerHTML += '<span class="symbol">°C</span>'
+  city.textContent = name
+  describe.textContent = description
+  feelslike.textContent = 'FEELS LIKE: ' + feels + '°C'
+  humidtext.textContent = 'HUMIDITY: ' + humidity + '%'
+  windspeed.textContent = 'WIND SPEED: ' + wind
+}
+
+
+function inputEventListener() {
+  const form = document.getElementById('form')
+  form.addEventListener('submit', handleSubmit)
+}
 function handleSubmit (e) {
   e.preventDefault();
   const input = document.getElementById('input')
@@ -24,9 +60,5 @@ function handleSubmit (e) {
   processData(country)
   input.value = '';
 }
-function inputEventListener() {
-  const form = document.getElementById('form')
-  form.addEventListener('submit', handleSubmit)
-}
-
 inputEventListener();
+fetchData('singapore');
